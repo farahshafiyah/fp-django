5eebf6fcd56dbf35bb916e0dc35976869aa4a2bd
farahshafiyah <farahs2maharani.23@gmail.com>
2023-01-20 10:44:29 +0700

    prt 13 create&read

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from dashboard.forms import FormBarang
 from dashboard.models import Barang, Brand
+from django.contrib import messages
 
 
 def tambah_barang(request):
-    form = FormBarang()
-    konteks = {
-        'form': form,
-    }
+    if request.POST:
+        form = FormBarang(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Success Add Data !")
+            form = FormBarang()
+            title = "Add Barang"
+            konteks = {
+            'form': form,
+            'titlenya': title,
+            }
+        return render(request, 'tambah_barang.html', konteks)
+    
+    else:
+        form = FormBarang()
+        title = "Add Barang"
+        konteks = {
+            'form': form,
+            'titlenya': title,
+        }
     return render(request, 'tambah_barang.html', konteks)
 
 
--- kelas/urls.py
+++ kelas/urls.py
@@ -5,22 +5,22 @@ from django.shortcuts import render
 from dashboard.views import dashboard, tambah_barang, Barang_View, Brand_View
 from category.views import category
 
-# def coba1(request):
-#     return HttpResponse('Selamat Datang !')
+def coba1(request):
+    return HttpResponse('Selamat Datang !')
 
 
-def coba2(request):
-    title = "Home"
-    konteks = {
-        'titlenya': title,
-    }
-    return render(request, 'home.html', konteks, {'navbar': 'home'})
+# def coba2(request):
+#     title = "Home"
+#     konteks = {
+#         'titlenya': title,
+#     }
+#     return render(request, 'home.html', konteks, {'navbar': 'home'})
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', coba1),
-    path('', coba2),
+    path('', coba1),
+    # path('', coba2),
     path('barang/', Barang_View),
     path('brand/', Brand_View),
     path('category/', category),
